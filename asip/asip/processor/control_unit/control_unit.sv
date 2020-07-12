14cfea4041cff44a17f3804a578dd20b0c45907c
nbaltodano <nestobaldo0412@gmail.com>
2020-07-12 17:41:39 -0600

    Control Unit signals correction

--- asip/asip/processor/control_unit/control_unit.sv
+++ asip/asip/processor/control_unit/control_unit.sv
@@ -4,12 +4,11 @@
  * Inputs:
  *     OpCode       - Instruction Operator Code.
  *     Function     - Instruction Function Code.
- *     Flags        - Flags input from ALU.
  *
  * Outputs:
- *     PC_Sel	 - 
- *     WR_En	 - 
+ *     Branch	 -  
  *     Ext_Sel	 - 
+ *     WR_En	 -
  *     Opb_Sel	 - 
  *     Alu_Func	 - 
  *     WD_Sel	 - 
@@ -18,22 +17,22 @@
  */
 
 module ControlUnit (
-	input logic [2:0] OpCode, Flags,
+	input logic [2:0] OpCode
 	input logic [1:0] Funct,
-	output logic [1:0] Ext_Sel,
-	output logic PC_Sel, WR_En, Opb_Sel, Alu_Func, WD_Sel, WM_En);
+	output logic [1:0] Branch, Ext_Sel,
+	output logic WR_En, Opb_Sel, Alu_Func, WD_Sel, WM_En);
 
 /*
 Controls:
-controls[0] = PC_Sel;
-controls[1] = WR_En;
+controls[1:0] = Branch;
 controls[3:2] = Ext_Sel[1:0];
-controls[4] = Opb_Sel;
-controls[5] = Alu_Func;
-controls[6] = WD_Sel;
-controls[7] = WM_En;
+controls[4] = WR_En;
+controls[5] =Opb_Sel;
+controls[6] = Alu_Func;
+controls[7] = WD_Sel;
+controls[8] = WM_En;
  */
-logic [7:0] controls;
+logic [8:0] controls;
 
 always_comb begin
 
@@ -44,19 +43,19 @@ always_comb begin
 		casex(Funct)
 
 			//Verificar si la instrucción es add
-			2'd0: controls = 8'b01000000;
+			2'd0: controls = 9'b110010000;
 
 			//Verificar si la instrucción es and inm
-			2'd1: controls = 8'b01001000;
+			2'd1: controls = 9'b110111000;
 
 			//Verificar si la instrucción es sub
-			2'd2: controls = 8'b01000100;
+			2'd2: controls = 9'b110010100;
 
 			//Verificar si la instrucción es sub inm
-			2'd3: controls = 8'b01001100;
+			2'd3: controls = 9'b110111100;
 			
 			//Valor default
-			default: controls = 8'b00000000;
+			default: controls = 9'b000000000;
 
 		endcase
 
@@ -65,13 +64,13 @@ always_comb begin
 		casex(Funct)
 
 			//Verificar si la instrucción es CMP-sub
-			2'd2: controls = 8'b00000100;
+			2'd2: controls = 9'b110000100;
 
 			//Verificar si la instrucción es CMP-sub inm
-			2'd3: controls = 8'b00001100;
+			2'd3: controls = 9'b110101100;
 			
 			//Valor default
-			default: controls = 8'b00000000;
+			default: controls = 9'b000000000;
 
 		endcase
 
@@ -83,44 +82,30 @@ always_comb begin
 		casex(OpCode)
 
 			//Verificar si la instrucción es LDR
-			3'd2: controls = 8'b01011010;
+			3'd2: controls = 9'b111011010;
 
 			//Verificar si la instrucción es STR
-			3'd3: controls = 8'b00011001;
+			3'd3: controls = 9'b111001001;
 
 			//Verificar si la instrucción es JEQ Label
-			3'd4: begin
-				//si Z = 1
-				if(Flgs == 1) begin
-					controls = 8'b10100000;
-				end else begin
-					controls = 8'b00100000;
-				end
-			end
+			3'd4: controls = 9'b001100000;
 
 			//Verificar si la instrucción es JNE Label
-			3'd5: begin
-				//si Z = 1
-				if(Flgs == 1) begin
-					controls = 8'b00100000;
-				end else begin
-					controls = 8'b10100000;
-				end
-			end
+			3'd5: controls = 9'b011100000;
 
 			//Verificar si la instrucción es JMP Label
-			3'd6: controls = 8'b10100000;
+			3'd6: controls = 9'b101100000;
 
 			//Verificar si la instrucción es Nop
-			3'd7: controls = 8'b00000000;
+			3'd7: controls = 9'b1111110000;
 
 			//Valor default
-			default: controls = 8'b00000000;
+			default: controls = 9'b000000000;
 
 		endcase 
 	end
 end
 
-assign {PC_Sel, WR_En, Ext_Sel, Opb_Sel, Alu_Func, WD_Sel, WM_En} = controls;
+assign {Branch, Ext_Sel, WR_En, Opb_Sel, Alu_Func, WD_Sel, WM_En} = controls;
 
 endmodule
