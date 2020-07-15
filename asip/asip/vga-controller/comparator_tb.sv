5e524035a47e32346a7ede93815a54b8dc52e7bd
nbaltodano <nestobaldo0412@gmail.com>
2020-07-15 15:48:28 -0600

    Comparator optimization

--- asip/asip/vga-controller/comparator_tb.sv
+++ asip/asip/vga-controller/comparator_tb.sv
@@ -1,15 +1,15 @@
 module comparator_tb;
 	logic [9:0] a, b, c, d, e;
 	
-	logic eq, neq, lt, lte, gt, gte, gte2, gte3;
+	logic a_lt_e, a_gte_b, a_gte2_c, a_gte3_c;
 	
 	initial begin
-		 a = 10'b0;
-		 b = 10'd6;
-         c = 10'd7;
-         d = 10'd2;
-         e = 10'd8;
+		 a = 10'd5;
+		 b = 10'd3;
+       c = 10'd5;
+       d = 10'd7;
+       e = 10'd8;
 	end
-	comparator#(10) co0(a, b, c, d, e, eq);
+	comparator#(10) co0(a, b, c, d, e, a_lt_e, a_gte_b, a_gte2_c, a_gte3_c);
 
 	endmodule
