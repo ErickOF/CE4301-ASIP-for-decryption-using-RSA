0ab64794bed11feb9691a0cfa2ac75a8a004afd1
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-07-15 15:17:53 -0600

    [Update] - Memory Stage

--- asip/asip/processor/path/mem-stage/mem_stage.sv
+++ asip/asip/processor/path/mem-stage/mem_stage.sv
@@ -8,10 +8,10 @@
  *     clk           - clock signal.
  * 	 write_enable  - write enable signal.
  * 	 address       - address in memory.
- *     write_data            - data to write.
+ *     write_data    - data to write.
  *
  * Outputs:
- *     read_data            - read data from memory.
+ *     read_data     - read data from memory.
  *
  */
  
@@ -20,18 +20,41 @@ module mem_stage #(parameter N=32)
                    input  logic [N-1:0] address, write_data, 
                    output logic [N-1:0] read_data);
 
-//create memory space
-						 
-logic [N-1:0] memory[0:204799];
+	// Signals
+	logic we0, we1, we2, we3;
+	// Read data
+	logic [N-1:0] rd0, rd1, rd2, rd3, rd;
 
+	// Write enable signals
+	assign we0 = (address[N-1:16]) == 16'b00 ? (1'b1 && write_enable) : 1'b0;
+	assign we1 = (address[N-1:16]) == 16'b01 ? (1'b1 && write_enable) : 1'b0;
+	assign we2 = (address[N-1:16]) == 16'b10 ? (1'b1 && write_enable) : 1'b0;
+	assign we3 = (address[N-1:16]) == 16'b11 ? (1'b1 && write_enable) : 1'b0;
 
-initial begin
-    $readmemh("Hexa.txt", memory);
-end 
+	// Memory Array
+	data_memory #(16, 32) dmem0(clk, we0,  address[15:0], write_data, rd0);
+	data_memory #(16, 32) dmem1(clk, we1,  address[15:0], write_data, rd1);
+	data_memory #(16, 32) dmem2(clk, we2,  address[15:0], write_data, rd2);
+	data_memory #(16, 32) dmem3(clk, we3,  address[15:0], write_data, rd3);
 
-assign read_data = memory[address[N-1:2]];
-
-always_ff @(posedge clk)
-    if (write_enable) memory[address[N-1:2]] <= write_data;	 
+	always_comb begin
+		if (address[N-1:16] == 0) begin
+			rd = rd0;
+		end
+		else if (address[N-1:16] == 1) begin
+			rd = rd1;
+		end
+		else if (address[N-1:16] == 2) begin
+			rd = rd2;
+		end
+		else if (address[N-1:16] == 3) begin
+			rd = rd3;
+		end
+		else begin
+			rd = 32'b0;
+		end
+	end
+	
+	assign read_data = rd;
 
 endmodule // mem_stage
--- asip/simulation/modelsim/asip_run_msim_rtl_verilog.do
+++ asip/simulation/modelsim/asip_run_msim_rtl_verilog.do
@@ -5,6 +5,7 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -sv -work work +incdir+C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/wb-stage {C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/wb-stage/wb_stage_test.sv}
-vlog -sv -work work +incdir+C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/wb-stage {C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/wb-stage/wb_stage.sv}
+vlog -sv -work work +incdir+C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/mem-stage {C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/mem-stage/mem_stage_test.sv}
+vlog -sv -work work +incdir+C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/mem-stage {C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/mem-stage/mem_stage.sv}
+vlog -sv -work work +incdir+C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/mem-stage {C:/Users/erick/Documents/Git/ASIP-for-decryption-using-RSA/asip/asip/processor/path/mem-stage/data_memory.sv}
 
